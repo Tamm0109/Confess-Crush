36deb0303b8d421dcaafc4607fec5b24bd79b7ae
Tamm0109 <106901578+Tamm0109@users.noreply.github.com>
2022-06-05 22:43:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Chuyện không kể nửa đêm",
+    introTitle: 'Hạt Thóc',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
